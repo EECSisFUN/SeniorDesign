4d2bad8ed7381183e1bf778c61a74bbe95d30b0a
maxdgoad <maxdgoad@gmail.com>
2021-04-05 16:01:12 -0500

    hmm

--- coms/Apps/Drawing.py
+++ coms/Apps/Drawing.py
@@ -2,10 +2,13 @@ import base64
 
 def saveImage(imageData):
     imageData.replace("image/octet-stream", "image/png")
-    if len(imageData) % 4:
-        #not a multiple of 4, add padding:
-        imageData += '=' * (4 - len(imageData) % 4) 
+    
     print(imageData)
-    with open("drawing.png", "wb") as file:
-        file.write(base64.b64decode(imageData))
+    byteData = bytes(imageData, "utf-8")
+    if len(byteData) % 4:
+        #not a multiple of 4, add padding:
+        byteData += b'=' * (4 - len(byteData) % 4) 
+    file = open("drawing.png", "w")
+    file.write(base64.decodestring(byteData))
+    file.close()
     
